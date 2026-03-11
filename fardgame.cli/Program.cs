a02c462ed5e12ecabb5f1d2713b52468368b20bc
ben <{username}@users.noreply.github.com>
2026-03-11 12:55:27 -0500

    configure DI and setup a game loop

--- fardgame.cli/Program.cs
+++ fardgame.cli/Program.cs
@@ -1,4 +1,6 @@
-﻿using fardgame.models;
+﻿using fardgame.cli.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace fardgame.cli
 {
@@ -6,9 +8,47 @@ namespace fardgame.cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GameInfo.GameName);
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-            Console.ReadLine();
+            // Add services
+            builder.Services.AddSingleton<GameLoopService>();
+
+            using (IHost host = builder.Build())
+            {
+                var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+                lifetime.ApplicationStarted.Register(() =>
+                {
+                    Console.WriteLine("Started");
+
+                    // Intro text
+                    Console.WriteLine("** FardGame CLI **");
+                    Console.WriteLine($"Today's Date: {DateTime.Now.ToShortDateString()}");
+                    Console.WriteLine($"Environment: {Environment.GetEnvironmentVariable("environment") ?? "!! none set"}");
+                    Console.WriteLine("******************");
+                    Console.WriteLine();
+
+                    // Inject the game loop singleton and start it
+                    using (var scope = host.Services.CreateScope())
+                    {
+                        var gameLoop = scope.ServiceProvider.GetRequiredService<GameLoopService>();
+                        gameLoop.StartGameLoop();
+                    }
+                });
+
+                lifetime.ApplicationStopping.Register(() =>
+                {
+                    Console.WriteLine("Stopping");
+                });
+
+                lifetime.ApplicationStopped.Register(() =>
+                {
+                    Console.WriteLine("Stopped");
+                });
+
+                host.Start();
+                host.WaitForShutdown();
+            }
         }
     }
 }
---
+++ fardgame.cli/Services/GameLoopService.cs
@@ -0,0 +1,19 @@
+﻿using fardgame.models;
+
+namespace fardgame.cli.Services
+{
+    internal class GameLoopService
+    {
+        public GameLoopService() { }
+
+        public void StartGameLoop()
+        {
+            Console.WriteLine("Game loop started");
+            while (true)
+            {
+                Console.WriteLine("game loop test - press enter to continue looping....");
+                Console.ReadLine();
+            }
+        }
+    }
+}
