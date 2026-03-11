35d92d3b5d91c49aa11ff241a1f656f6483306fc
ben <{username}@users.noreply.github.com>
2026-03-11 12:23:01 -0500

    init cli project

---
+++ fardgame.cli/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace fardgame.cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fard Game");
+
+            Console.ReadLine();
+        }
+    }
+}
