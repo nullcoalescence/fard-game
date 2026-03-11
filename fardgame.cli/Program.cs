0b4936d427a65fd2e26334af0653f6dda92ff3e8
ben <{username}@users.noreply.github.com>
2026-03-11 12:30:29 -0500

    init models project

--- fardgame.cli/Program.cs
+++ fardgame.cli/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace fardgame.cli
+﻿using fardgame.models;
+
+namespace fardgame.cli
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fard Game");
+            Console.WriteLine(GameInfo.GameName);
 
             Console.ReadLine();
         }
